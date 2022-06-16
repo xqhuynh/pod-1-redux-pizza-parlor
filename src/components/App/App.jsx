edea56153621bfd9e848e78a594c1ca1bd608a62
Savon Huynh <xqhuynh@gmail.com>
2022-06-16 10:35:37 -0500

    Create GET route to get pizzaList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+// import components
+import PizzaList from '../Home/Home';
 
 function App() {
+  // call useDispatch 
+  const dispatch = useDispatch();
+  // bring in useEffect to display pizza list 'fetchPizza(); when page loads
+  useEffect(() => {fetchPizza(); }, [])
+
+  // GET route to display pizzas, /api/pizza
+  const fetchPizza = () => {
+    axios({
+      method: 'GET', 
+      url: '/api/pizza'
+    })
+    .then((response) => {
+      console.log('GET /api/piza list is', response);
+      dispatch({
+        type: 'SET_PIZZA_LIST',
+        payload: response.data
+      })
+    })
+    .catch((err) => {
+      console.log('Error getting pizza list', response);
+    })
+  }
 
   return (
     <div className='App'>
@@ -12,7 +39,7 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <PizzaList />  
     </div>
   );
 }
