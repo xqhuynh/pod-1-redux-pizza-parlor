4f5dab279c0d1b392e2a8addbfc8fb896e99b523
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-16 10:45:31 -0500

    wireframe radio added

--- src/components/CustomerInfoForm/CustomerInfoForm.jsx
+++ src/components/CustomerInfoForm/CustomerInfoForm.jsx
@@ -3,14 +3,24 @@ function CustomerInfoForm() {
     return (
         <>
             <form>
-                <h2>Step 2: Customer Information</h2>
+                <div>
+                    <h3>Step 2: Customer Information</h3>
 
-                
+
+                        <input type="radio" id="pickup" name="pickup_or_delivery"
+                    value="Pickup"/>
+                        <label htmlFor="pickup">Pickup</label><br/>
+                        <input type="radio" id="delivery" name="pickup_or_delivery"
+                    value="Delivery"/>
+                        <label htmlFor="delivery">Delivery</label><br/>
+
                     <input id="name" type="text" placeholder="Name" />
                     <input id="address" placeholder="Street Address" />
                     <input id="city" type="text" placeholder="City" />
                     <input id="zip" type="number" placeholder="Zip" />
-            
+
+                </div>
+                <button type="submit">NEXT</button>
             </form>
         </>
     )
