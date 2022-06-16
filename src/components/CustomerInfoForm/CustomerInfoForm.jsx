b87a1c1e8508834ddb43f7698b5ff39fc0aa9e63
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-16 10:53:25 -0500

    inital commit

--- src/components/CustomerInfoForm/CustomerInfoForm.jsx
+++ src/components/CustomerInfoForm/CustomerInfoForm.jsx
@@ -7,12 +7,12 @@ function CustomerInfoForm() {
                     <h3>Step 2: Customer Information</h3>
 
 
-                        <input type="radio" id="pickup" name="pickup_or_delivery"
-                    value="Pickup"/>
-                        <label htmlFor="pickup">Pickup</label><br/>
-                        <input type="radio" id="delivery" name="pickup_or_delivery"
-                    value="Delivery"/>
-                        <label htmlFor="delivery">Delivery</label><br/>
+                    <input type="radio" id="pickup" name="pickup_or_delivery"
+                        value="Pickup" />
+                    <label htmlFor="pickup">Pickup</label><br />
+                    <input type="radio" id="delivery" name="pickup_or_delivery"
+                        value="Delivery" />
+                    <label htmlFor="delivery">Delivery</label><br />
 
                     <input id="name" type="text" placeholder="Name" />
                     <input id="address" placeholder="Street Address" />
