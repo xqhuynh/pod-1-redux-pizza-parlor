e85dc45df63e700a06401163eb22c1141b8f0d82
Savon Huynh <xqhuynh@gmail.com>
2022-06-17 07:38:53 -0500

    Style pizza item boxes

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,25 +1,38 @@
 import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function PizzaList() {
-    // Bring in useSelector
-    const pizzaList = useSelector(store => store.pizzaList);
+  // Bring in useSelector
+  const pizzaList = useSelector((store) => store.pizzaList);
 
-    return (
-        <>
-            <h3>Pizza List Test</h3>
-            <div className="pizza-container">
-                {pizzaList.map((pizza) => 
-                <div className="pizza-card"key={pizza.id}>
-                  <img className="picture" src={pizza.image_path} />
-                    <h3 className="pizza-name">{pizza.name}</h3>
-                    <p>{pizza.description}</p>
-                    <p className="pizza-price">{pizza.price}</p>
-                    <button className="pizza-button">Add</button>
-                </div>)}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="title-container">
+        <h3 className="step-one">Step 1: Select Your Pizza</h3>
+      </div>
+      <div className="pizza-container">
+        {pizzaList.map((pizza) => (
+          <div key={pizza.id}>
+            <Card border="primary" style={{ width: "16rem", height: "100%" }}>
+              <Card.Img variant="top" src={pizza.image_path} />
+              <Card.Body>
+                <Card.Title>{pizza.name}</Card.Title>
+                <Card.Text className="description-text">
+                  {pizza.description}
+                </Card.Text>
+              </Card.Body>
+              <Card.Text className="price-text">${pizza.price}</Card.Text>
+              <Button size="md" variant="primary">
+                Add
+              </Button>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default PizzaList;
